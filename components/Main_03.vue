b8858f59f11b36f00f24e61630492e33d51b092e
r4nd3l <matemolnar88@gmail.com>
2024-02-29 16:07:29 +0100

    init commit

---
+++ components/Main_03.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid grid-col-12">
+    <div class="col-span-12">
+      <div
+        class="flex flex-col justify-center items-center border-2 border-red-500 my-2"
+      >
+        <div>
+          <ContentDoc path="main/block_03" v-slot="{ doc }">
+            <small>{{ doc.description }}</small>
+            <ContentRenderer :value="doc" />
+          </ContentDoc>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLayout name="base">
+    <Main_01 />
+    <Main_02 />
+    <Main_03 />
+    <Main_04 />
+    <Main_05 />
+  </NuxtLayout>
+</template>
